c1fbc12a0cb0dee041b960c16410501e0260f328
Willy Aguirre <marti1125@gmail.com>
2019-01-19 17:09:15 -0500

    se agrega integracion con mapbox

--- app/src/main/java/passenger/creation/symbio/app/symbiocreationpassenger/MainActivity.java
+++ app/src/main/java/passenger/creation/symbio/app/symbiocreationpassenger/MainActivity.java
@@ -1,16 +1,30 @@
 package passenger.creation.symbio.app.symbiocreationpassenger;
 
+import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.Toast;
 
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
 
     private MapView mvRoute;
+    private MapboxMap map;
+    private PermissionsManager permissionsManager;
+
+    private Location originLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mvRoute = findViewById(R.id.mv_route);
         mvRoute.onCreate(savedInstanceState);
-        mvRoute.getMapAsync(mapbox -> {
-            mapbox.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-                @Override
-                public void onStyleLoaded(@NonNull Style style) {
-
-                }
-            });
-        });
+        mvRoute.getMapAsync(this);
     }
 
     @Override
@@ -71,4 +78,52 @@ public class MainActivity extends AppCompatActivity {
         mvRoute.onSaveInstanceState(outState);
     }
 
+
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        this.map = mapboxMap;
+        enableLocationComponent();
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent() {
+
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            LocationComponent locationComponent = map.getLocationComponent();
+            locationComponent.activateLocationComponent(this);
+            locationComponent.setLocationComponentEnabled(true);
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+            originLocation = locationComponent.getLastKnownLocation();
+
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            enableLocationComponent();
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
 }
+
